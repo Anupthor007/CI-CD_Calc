c38cc100fdc2b8c8283612cc43d90c0576fce4f4
Anupthor007 <anupthorat060@gmail.com>
2026-06-25 15:10:07 +0530

    fixed actions issue

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,7 +4,7 @@ def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
-    assert subtract(5, 2) == 2
+    assert subtract(5, 2) == 3
 
 def test_multiply():
     assert multiply(2, 4) == 8
